ab6d9b8ca027aaaf8fdd8b4d23ee566173022680
DanhDue ExOICTIF <danhdue@gmail.com>
2026-09-02 14:09:04 +0700

    Initial Commit

---
+++ iOSDigitalWallet/iOSDigitalWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDigitalWalletApp.swift
+//  iOSDigitalWallet
+//
+//  Created by DanhDue ExOICTIF on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct iOSDigitalWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSDigitalWalletTests/iOSDigitalWalletTests.swift
@@ -0,0 +1,18 @@
+//
+//  iOSDigitalWalletTests.swift
+//  iOSDigitalWalletTests
+//
+//  Created by DanhDue ExOICTIF on 9/2/26.
+//
+
+import Testing
+
+struct iOSDigitalWalletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
